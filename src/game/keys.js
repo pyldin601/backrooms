513afcc58190872d43f0617d75cd844da8dca107
Roman <roman.lakhtadyr@gmail.com>
2018-06-24 01:34:05 +0300

    create keys stream source

---
+++ src/game/keys.js
@@ -0,0 +1,33 @@
+// @flow
+import { Observable, fromEvent, merge } from 'rxjs';
+import { distinctUntilChanged, groupBy, map, mergeAll, scan, tap, filter } from 'rxjs/operators';
+
+export type KeysStateInterface = {|
+  ArrowLeft: boolean,
+  ArrowRight: boolean,
+  ArrowUp: boolean,
+  ArrowDown: boolean,
+|};
+
+const initialKeysState = {
+  ArrowLeft: false,
+  ArrowRight: false,
+  ArrowUp: false,
+  ArrowDown: false,
+};
+
+const allowedKeys = new Set(['ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown']);
+
+export function createKeysStream(): Observable<KeysStateInterface> {
+  const downs$ = fromEvent(document, 'keydown');
+  const ups$ = fromEvent(document, 'keyup');
+
+  return merge(downs$, ups$).pipe(
+    tap(event => event.preventDefault()),
+    filter(event => allowedKeys.has(event.key)),
+    groupBy(event => event.key),
+    map(group => group.pipe(distinctUntilChanged(null, e => e.type))),
+    mergeAll(),
+    scan((acc, event) => ({ ...acc, [event.key]: event.type === 'keydown' }), initialKeysState),
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 // @flow
-import { interval } from 'rxjs';
 import initialState from './game/initial-state';
 import render from './game/render';
+import { createKeysStream } from './game/keys';
 
 const canvas = document.getElementById('canvas-main');
 
 if (canvas instanceof HTMLCanvasElement) {
   const context = canvas.getContext('2d');
-  interval(1000).subscribe(() => render(context, initialState));
+  createKeysStream().subscribe(key => console.log(key));
+  // interval(1000).subscribe(() => render(context, initialState));
 }
