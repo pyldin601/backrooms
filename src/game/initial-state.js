1b22ffe639cd8c92dfc7a3faab7bc4b13d2e1355
Roman <roman.lakhtadyr@gmail.com>
2018-06-24 17:45:13 +0300

    render walls

--- src/game/initial-state.js
+++ src/game/initial-state.js
@@ -11,7 +11,7 @@ const initialState: GameStateInterface = {
   },
   map: {
     walls: [
-      { x1: -20, y1: 20, x2: 20, y2: 20 },
+      { x1: 20, y1: 20, x2: 280, y2: 20 },
     ],
   },
 };
--- src/game/render.js
+++ src/game/render.js
@@ -6,7 +6,7 @@ import type { Point } from '../util/geometry';
 const moveTo = (context, { x, y }: Point) => context.moveTo(x, y);
 const lineTo = (context, { x, y }: Point) => context.lineTo(x, y);
 
-function renderPlayer(context: CanvasRenderingContext2D, player: PlayerStateInterface) {
+function renderPlayer(context: CanvasRenderingContext2D, { player } : GameStateInterface) {
   const { x, y, angle } = player.position;
 
   const path = new Path2D();
@@ -18,11 +18,21 @@ function renderPlayer(context: CanvasRenderingContext2D, player: PlayerStateInte
   context.stroke(path);
 }
 
-function renderMap(context: CanvasRenderingContext2D, map: MapStateInterface) {
-  console.log('map', map);
+function renderMap(context: CanvasRenderingContext2D, { map }: GameStateInterface) {
+  const path = new Path2D();
+
+  map.walls.forEach(wall => {
+    console.log(wall);
+    moveTo(path, { x: wall.x1, y: wall.y1 });
+    lineTo(path, { x: wall.x2, y: wall.y2 });
+  });
+
+  context.strokeStyle = '#aabb00';
+  context.stroke(path);
 }
 
 export default function render(context: CanvasRenderingContext2D, game: GameStateInterface) {
   context.clearRect(0, 0, 300, 300);
-  renderPlayer(context, game.player);
+  renderMap(context, game);
+  renderPlayer(context, game);
 }
