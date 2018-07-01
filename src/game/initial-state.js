12d6973d63b4a255332ab9ababffd520603bb963
Roman <roman.lakhtadyr@gmail.com>
2018-07-01 21:00:41 +0300

    restore initial state

--- src/game/initial-state.js
+++ src/game/initial-state.js
@@ -6,15 +6,15 @@ import type { GameStateInterface } from "./state";
 const WALL_SIZE = 10;
 
 const initialMapPoints = [
-  // {x: CANVAS_WIDTH / 2 - 20, y: CANVAS_HEIGHT / 2 - 20},
-  // {x: CANVAS_WIDTH / 2 - 10, y: CANVAS_HEIGHT / 2 - 20},
-  // {x: CANVAS_WIDTH / 2 - 10, y: CANVAS_HEIGHT / 2 - 10},
-  // {x: CANVAS_WIDTH / 2 + 10, y: CANVAS_HEIGHT / 2 - 10},
-  // {x: CANVAS_WIDTH / 2 + 10, y: CANVAS_HEIGHT / 2 - 20},
-  // {x: CANVAS_WIDTH / 2 + 20, y: CANVAS_HEIGHT / 2 - 20},
-  // {x: CANVAS_WIDTH / 2 + 20, y: CANVAS_HEIGHT / 2 + 10},
-  // {x: CANVAS_WIDTH / 2 + 10, y: CANVAS_HEIGHT / 2 + 20},
-  // {x: CANVAS_WIDTH / 2 - 10, y: CANVAS_HEIGHT / 2 + 20},
+  {x: CANVAS_WIDTH / 2 - 20, y: CANVAS_HEIGHT / 2 - 20},
+  {x: CANVAS_WIDTH / 2 - 10, y: CANVAS_HEIGHT / 2 - 20},
+  {x: CANVAS_WIDTH / 2 - 10, y: CANVAS_HEIGHT / 2 - 10},
+  {x: CANVAS_WIDTH / 2 + 10, y: CANVAS_HEIGHT / 2 - 10},
+  {x: CANVAS_WIDTH / 2 + 10, y: CANVAS_HEIGHT / 2 - 20},
+  {x: CANVAS_WIDTH / 2 + 20, y: CANVAS_HEIGHT / 2 - 20},
+  {x: CANVAS_WIDTH / 2 + 20, y: CANVAS_HEIGHT / 2 + 10},
+  {x: CANVAS_WIDTH / 2 + 10, y: CANVAS_HEIGHT / 2 + 20},
+  {x: CANVAS_WIDTH / 2 - 10, y: CANVAS_HEIGHT / 2 + 20},
   {x: CANVAS_WIDTH / 2 - 20, y: CANVAS_HEIGHT / 2 + 10},
   {x: CANVAS_WIDTH / 2 - 20, y: CANVAS_HEIGHT / 2 - 20},
 ];
