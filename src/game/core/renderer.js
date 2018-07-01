e19e4d8628189c0ee7322399e0b585cbc5ecdb89
Roman <roman.lakhtadyr@gmail.com>
2018-07-02 00:04:30 +0300

    fix lens skew

--- src/game/core/renderer.js
+++ src/game/core/renderer.js
@@ -11,6 +11,7 @@ export function renderColumn(
   context: CanvasRenderingContext2D,
   sector: Sector,
   ray: Ray,
+  camera: Camera,
   screenOffset: number,
   rayWidth: number,
 ) {
@@ -24,13 +25,14 @@ export function renderColumn(
 
     nearestWall = rayCross.distance;
 
-    const wallViewHeight = CANVAS_HEIGHT / rayCross.distance;
+    const lensDistance = rayCross.distance * Math.cos(camera.angle - ray.angle);
+    const perspectiveHeight = CANVAS_HEIGHT / lensDistance * 0.5;
 
     context.save();
 
     context.beginPath();
     context.fillStyle = darken(wall.color, rayCross.distance);
-    context.fillRect(screenOffset, CANVAS_HEIGHT / 2 - wallViewHeight, rayWidth, wallViewHeight * 2);
+    context.fillRect(screenOffset, CANVAS_HEIGHT / 2 - perspectiveHeight, rayWidth, perspectiveHeight * 2);
     context.closePath();
     context.fill();
 
@@ -43,6 +45,6 @@ export function renderSector(context: CanvasRenderingContext2D, sector: Sector, 
 
   for (let i = 0; i < CANVAS_WIDTH; i += RAY_WIDTH) {
     const ray = { ...camera, angle: startAngle + (FOV_IN_RADIANS / CANVAS_WIDTH) * i };
-    renderColumn(context, sector, ray, i * RAY_WIDTH, RAY_WIDTH);
+    renderColumn(context, sector, ray, camera, i * RAY_WIDTH, RAY_WIDTH);
   }
 }
