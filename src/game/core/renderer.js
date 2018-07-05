95bdc2d99a0d9c831e70ce3398e006eeefa27bf1
Roman <roman.lakhtadyr@gmail.com>
2018-07-05 21:28:44 +0300

    refactoring

--- src/game/core/renderer.js
+++ src/game/core/renderer.js
@@ -1,5 +1,5 @@
 // @flow
-import type { Sector, Camera, Ray } from './types';
+import type { Sector, Camera, Ray, Wall, RayCross } from './types';
 import { PERSPECTIVE_WIDTH, PERSPECTIVE_HEIGHT } from '../../consts';
 import { crossTheWall, moveAndRotateCamera } from './raycaster';
 import { darken } from './colors';
@@ -8,7 +8,70 @@ import { getWallAngle, getWallCenter } from '../../util/geometry';
 export const FOCUS_LENGTH = 0.8;
 export const HEIGHT_RATIO = 1.3;
 
-export const RENDER_ALL_WALLS = false;
+function renderPortal(
+  wall: Wall,
+  sectors: Sector[],
+  context: CanvasRenderingContext2D,
+  ray: Camera,
+  camera: Camera,
+  screenOffset: number,
+  screenWidth: number,
+) {
+  const portal = wall.portal;
+
+  const thatWall = sectors[wall.portal.sector].walls[wall.portal.wall];
+
+  const thisWallAngle = getWallAngle(wall);
+  const thatWallAngle = getWallAngle(thatWall);
+
+  const thisWallCenter = getWallCenter(wall);
+  const thatWallCenter = getWallCenter(thatWall);
+
+  const angleDiff = thisWallAngle - thatWallAngle;
+  const moveX = thisWallCenter.x - thatWallCenter.x;
+  const moveY = thisWallCenter.y - thatWallCenter.y;
+
+  renderColumn(
+    context,
+    portal.sector,
+    sectors,
+    moveAndRotateCamera(ray, -moveX, -moveY, -angleDiff, thatWallCenter),
+    moveAndRotateCamera(camera, -moveX, -moveY, -angleDiff, thatWallCenter),
+    screenOffset,
+    screenWidth,
+  );
+}
+
+function renderWall(
+  rayCross: RayCross,
+  camera: Camera,
+  ray: Camera,
+  sectors: Sector[],
+  sector: number,
+  context: CanvasRenderingContext2D,
+  wall: Wall,
+  screenOffset: number,
+  screenWidth: number,
+) {
+  const lensDistance = rayCross.distance * Math.cos(camera.angle - ray.angle);
+  const perspectiveHeight =
+    (PERSPECTIVE_HEIGHT / lensDistance) * (sectors[sector].height / HEIGHT_RATIO);
+
+  context.save();
+
+  context.beginPath();
+  context.fillStyle = darken(wall.color, Math.sqrt(rayCross.distance) * 6);
+  context.fillRect(
+    screenOffset,
+    PERSPECTIVE_HEIGHT / 2 - perspectiveHeight,
+    screenWidth,
+    perspectiveHeight * 2,
+  );
+  context.closePath();
+  context.fill();
+
+  context.restore();
+}
 
 export function renderColumn(
   context: CanvasRenderingContext2D,
@@ -23,54 +86,16 @@ export function renderColumn(
   for (const wall of sectors[sector].walls) {
     const rayCross = crossTheWall(ray, wall);
 
-    if (rayCross === null || (!RENDER_ALL_WALLS && rayCross.distance >= nearestWall)) {
+    if (rayCross === null || rayCross.distance >= nearestWall) {
       continue;
     }
 
     nearestWall = rayCross.distance;
 
-    // If wall has portal render column of neighbor sector
-    if (wall.portal !== undefined && wall.portal !== null) {
-      const thatWall = sectors[wall.portal.sector].walls[wall.portal.wall];
-
-      const thisWallAngle = getWallAngle(wall);
-      const thatWallAngle = getWallAngle(thatWall);
-
-      const thisWallCenter = getWallCenter(wall);
-      const thatWallCenter = getWallCenter(thatWall);
-
-      const angleDiff = thisWallAngle - thatWallAngle;
-      const moveX = thisWallCenter.x - thatWallCenter.x;
-      const moveY = thisWallCenter.y - thatWallCenter.y;
-
-      renderColumn(
-        context,
-        wall.portal.sector,
-        sectors,
-        moveAndRotateCamera(ray, -moveX, -moveY, -angleDiff, thatWallCenter),
-        moveAndRotateCamera(camera, -moveX, -moveY, -angleDiff, thatWallCenter),
-        screenOffset,
-        screenWidth,
-      );
+    if (wall.portal) {
+      renderPortal(wall, sectors, context, ray, camera, screenOffset, screenWidth);
     } else {
-      const lensDistance = rayCross.distance * Math.cos(camera.angle - ray.angle);
-      const perspectiveHeight =
-        (PERSPECTIVE_HEIGHT / lensDistance) * (sectors[sector].height / HEIGHT_RATIO);
-
-      context.save();
-
-      context.beginPath();
-      context.fillStyle = darken(wall.color, Math.sqrt(rayCross.distance) * 6);
-      context.fillRect(
-        screenOffset,
-        PERSPECTIVE_HEIGHT / 2 - perspectiveHeight,
-        screenWidth,
-        perspectiveHeight * 2,
-      );
-      context.closePath();
-      context.fill();
-
-      context.restore();
+      renderWall(rayCross, camera, ray, sectors, sector, context, wall, screenOffset, screenWidth);
     }
   }
 }
