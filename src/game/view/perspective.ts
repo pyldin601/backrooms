108eeb1fff0d054d9b682391e101648b58717029
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2024-06-17 14:27:20 +0100

    Convert perspective.js to typescript

--- src/game/view/perspective.ts
+++ src/game/view/perspective.ts
@@ -1,17 +1,18 @@
 import { renderSector } from '../core/renderer';
+import { IGameState } from '@/game/map-types';
 
 function renderMap(
-  context,
-  { map, player },
-  textureImage,
+  context: CanvasRenderingContext2D,
+  { map, player }: IGameState,
+  textureImage: CanvasImageSource,
 ) {
   renderSector(context, player.position.sectorId, map.sectors, player.position, textureImage);
 }
 
 export default function render(
-  context,
-  game,
-  textureImage,
+  context: CanvasRenderingContext2D,
+  game: IGameState,
+  textureImage: CanvasImageSource,
 ) {
   renderMap(context, game, textureImage);
 }
