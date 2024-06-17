8c09e80c1d7b8d579fea901c5234455591ead6f7
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2024-06-17 14:34:54 +0100

    Convert keys.js to typescript

---
+++ src/game/keys.ts
@@ -0,0 +1,37 @@
+import { Observable, fromEvent, merge, distinct } from 'rxjs';
+import {
+  distinctUntilChanged,
+  groupBy,
+  map,
+  mergeAll,
+  scan,
+  tap,
+  filter,
+  startWith,
+} from 'rxjs/operators';
+
+const initialKeysState = {
+  ArrowLeft: false,
+  ArrowRight: false,
+  ArrowUp: false,
+  ArrowDown: false,
+  Alt: false,
+  Shift: false,
+};
+
+const allowedKeys = new Set(Object.keys(initialKeysState));
+
+export function createKeysStream() {
+  const downs$ = fromEvent<KeyboardEvent>(document, 'keydown');
+  const ups$ = fromEvent<KeyboardEvent>(document, 'keyup');
+
+  return merge(downs$, ups$).pipe(
+    filter((event) => allowedKeys.has(event.key)),
+    tap((event) => event.preventDefault()),
+    groupBy((event) => event.key),
+    map((group) => group.pipe(distinct((event) => event.type))),
+    mergeAll(),
+    scan((acc, event) => ({ ...acc, [event.key]: event.type === 'keydown' }), initialKeysState),
+    startWith(initialKeysState),
+  );
+}
