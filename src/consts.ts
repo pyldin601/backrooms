e1a58e65fc01057d2419a8e2b4a81e5b69ab911e
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2024-06-19 18:49:15 +0100

    Fix aspect ratio

--- src/consts.ts
+++ src/consts.ts
@@ -6,8 +6,8 @@ export const PERSPECTIVE_WIDTH = 800;
 export const PERSPECTIVE_HEIGHT = 400;
 
 // PLAYER MOVING CONSTS
-export const PLAYER_SPEED = .4;
-export const PLAYER_TURN_STEP = .03;
+export const PLAYER_SPEED = 0.4;
+export const PLAYER_TURN_STEP = 0.03;
 
 export const TEXTURE_MAP_SCALE = 6.4;
 export const TEXTURE_TILE_WIDTH = 64;
